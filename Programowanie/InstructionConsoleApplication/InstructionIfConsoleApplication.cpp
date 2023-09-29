b837646f5852837b5e5870c39ecd909a9848bd31
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-29 13:15:49 +0200

    jksd

---
+++ Programowanie/InstructionConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>;
+
+using namespace std;
+
+//Napisz funkcje która podzieli dwie liczby
+
+void task1()
+{
+	double FirstNumber, SecondNumber;
+		std::cout << "Podaj pierwszą liczbe:";
+		std::cin >> FirstNumber;
+		std::cout << "Podaj drugą liczbe:";
+		std::cin >> SecondNumber;
+
+		double quotient = FirstNumber / SecondNumber;
+
+		std::cout << "Wynik dzielenia :" << quotient << "\n";
+}
+
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	
+	task1();
+}
