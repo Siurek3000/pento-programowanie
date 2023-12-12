a8c9648db4c3e9d77a6d38722a1865ccf81997f6
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-12 16:44:54 +0100

    uu

---
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void task1()
+{
+	const int SIZE_OF_ARRAY = 5;
+	int numbers[SIZE_OF_ARRAY];
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+	{
+		cout << "Podajliczbe\n";
+		cin >> numbers[i];
+	}
+
+	for (int i = SIZE_OF_ARRAY - 1; i >= 0; i--)
+		cout << "Podana liczba" << numbers[i] << "\n";
+
+}
+
+
+
+
+
+int main()
+{
+	task1();
+}
+
