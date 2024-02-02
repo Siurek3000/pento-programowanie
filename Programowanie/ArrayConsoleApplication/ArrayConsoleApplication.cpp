32e3e3b02e83a9f826322b55ac6a7ba46990a5b6
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-02-02 13:16:18 +0100

    h

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
-using namespace std;
 
+//Napisz program, który wczyta 5 liczb a następnie je wyświetli na konsoli
+//w odwrotnej kolejności
 void task1()
 {
 	const int SIZE_OF_ARRAY = 5;
@@ -8,21 +9,38 @@ void task1()
 
 	for (int i = 0; i < SIZE_OF_ARRAY; i++)
 	{
-		cout << "Podajliczbe\n";
-		cin >> numbers[i];
+		std::cout << "Podaj liczbe\n";
+		std::cin >> numbers[i];
 	}
 
 	for (int i = SIZE_OF_ARRAY - 1; i >= 0; i--)
-		cout << "Podana liczba" << numbers[i] << "\n";
-
+		std::cout << "Podana liczba " << numbers[i] << "\n";
 }
 
+//Napisz program, który uzupełni tablicę liczbami losowymi 
+//a następnie znajdzie minimum oraz maksimum.
+void task2()
+{
+	const int UPPER_RANGE = 100;
+	const int LOWER_RANGE = 5;
 
+	const int SIZE_OF_ARRAY = 5;
+	int numbers[SIZE_OF_ARRAY];
 
+	srand(time(NULL));
 
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+	{
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+	}
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++)
+		std::cout << numbers[i] << ", ";
+	std::cout << "\n";
+
+}
 
 int main()
 {
-	task1();
+	task2();
 }
-
---
+++ Programowanie/StructConsoleApplication/StructConsoleApplication.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+struct point
+{
+    int x;
+    int y;
+    int z;
+};
+
+double distanceFromCenter(point p)
+{
+   
+    double distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+    return distance;
+}
+
+double distanceFromCenter(int x, int y, int z)
+{
+
+    double distance = sqrt(x * x + y * y + z * z);
+    return distance;
+}
+
+int main()
+{
+    int x, y, z;
+    
+    cout << "Podaj x\n";
+    cin >> x;
+    cout << "Podaj y\n";
+    cin >> y;
+    cout << "Podaj Z\n";
+    cin >> z;
+
+    cout << "Odleglosc" << distanceFromCenter(x, y, z) << "\n";
+    int x1, y1;
+    int x2, y2;
+
+    int xx[50], yy[50], zz[50];
+
+    xx[3] = 5;
+    yy[3] = 7;
+    zz[3] = 77;
+
+    point firstPoint;
+    firstPoint.x = 3;
+    firstPoint.y = 5;
+    firstPoint.z = 8;
+    cout << "Odległosc" << distanceFromCenter(firstPoint.x, firstPoint.y, firstPoint.z);
+    cout << "Odległosc" << distanceFromCenter(firstPoint) << "\n";
+}
+
