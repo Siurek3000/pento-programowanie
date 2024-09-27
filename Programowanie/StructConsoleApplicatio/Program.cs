567cd4515b2d08329fdb9681241a78a59d418f88
q <q>
2024-09-27 11:39:40 +0200

    ee

---
+++ Programowanie/StructConsoleApplicatio/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+Point firstPoint = new Point();
+firstPoint.x = 3;
+firstPoint.x = 10;
+Point secondPoint = firstPoint;
+secondPoint.y = 6;
+secondPoint.y = 99;
+Console.WriteLine(firstPoint.x);
+Console.WriteLine(secondPoint.y);
+
